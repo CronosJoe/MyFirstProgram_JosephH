6125a01fde8db703620e1f8bc92743e4bea61d66
Joseph Huskey <josephhuskey@Josephs-MacBook-Air.local>
2020-08-12 17:32:41 -0700

    Added the rest of the files(Jumped ahead and only pushed the VS file)

---
+++ HelloWorldAIEFirstClass/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorldAIEFirstClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Joseph, this is my first AIE program!");
+            Console.ReadLine();
+        }
+    }
+}
