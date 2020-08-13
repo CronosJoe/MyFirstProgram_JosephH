3520cd09731c7dea33364f5fcf5424e68d4ad3e6
Joseph Huskey <josephhuskey@Josephs-MacBook-Air.local>
2020-08-13 11:12:24 -0700

    Completed second day variable tutorial

---
+++ AIEDay2Variables/AIEDay2Variables/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AIEDay2Variables
+{
+    class Program
+    {
+        static void Main()
+        {
+            string greeting = "Hello AIE Day 2!";
+            int playerScore = 0;
+            bool gameStart = false;
+            Console.WriteLine(greeting + " Your Score is: " + playerScore);
+            Console.ReadLine();
+
+            string displayScore = " Your current Score is: " + playerScore + "."; /* note to self, this variable is saved now if playerScore changes after this this will not change */
+            Console.WriteLine(displayScore);
+
+        }
+    }
+}
