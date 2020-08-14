27b0974cfca7530c20a2fcf12493a5b4bb9c88a3
Joseph Huskey <josephhuskey@Josephs-MacBook-Air.local>
2020-08-14 11:57:37 -0700

    Finished fucntion exercise.

---
+++ FunctionsExercise/FunctionsExercise/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace FunctionsExercise
+{
+    public class Game
+    {
+      public int score;
+
+
+        public void Start()
+        {
+            Console.WriteLine($"Player Score is {score}.");
+            
+
+        }
+        public int AddToScore(int add)
+        {
+            score += add;
+            return score;
+        }
+        public void PrintScore(int data)
+        {
+            Console.WriteLine($"You have scored {data} points! Your new score is {AddToScore(data)}!");
+        }
+    }
+    
+    
+}
---
+++ FunctionsExercise/FunctionsExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FunctionsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.score = 100;
+            game.Start();
+            int count = 0;
+            int add = 30;
+            while (count < 3) //I didn't like repeating the same function call 3 times so I just put it into a loop and replaced some of the hard coding.
+            {
+                game.PrintScore(add);
+                add += add;
+                count++;
+            }
+           
+            
+        }
+    }
+}
