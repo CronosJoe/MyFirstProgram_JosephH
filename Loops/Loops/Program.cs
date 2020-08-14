8773d770a1914b288d8a02ea25805c40dadfa64d
Joseph Huskey <josephhuskey@Josephs-MacBook-Air.local>
2020-08-13 18:17:02 -0700

    Added the loops tutorial project, once again broke the while/for loop portions up into their own methods.

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // WhileLoops();
+            ForLoops();
+            Console.ReadKey();
+        }
+        static void WhileLoops()
+        {
+            int count = 0;
+            while (count < 3)
+            {
+                count++;
+                Console.WriteLine("The while loop has run " + count + " times."); //alternate solution is to put $ in front and putting brackets around the variable
+            }
+        }
+        static void ForLoops()
+        {
+            for (int count = 3; count >=1; count-=2)
+            {
+                Console.WriteLine($"The variable count now is equal to {count}.");
+            }
+        }
+    }
+}
