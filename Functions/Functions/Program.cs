19b5ef2bc4cf72c14f3fcd0d944b2811b077afd7
Joseph Huskey <josephhuskey@Josephs-MacBook-Air.local>
2020-08-14 10:31:52 -0700

    Added the functions tutorial

---
+++ Functions/Functions/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Functions
+{
+    class Program
+    {
+        static int playerHealth = 100;
+        static int monsterDamage = 30;
+
+        static void Main(string[] args)
+        {
+            Start();
+        }
+        static void Start()
+        {
+            Console.WriteLine("The game has started.");
+            Console.ReadKey();
+            MonsterEncounter();
+            MonsterAttacks(monsterDamage*2);
+            MonsterAttacks(monsterDamage);
+            MonsterAttacks(monsterDamage*3);
+        }
+        static void MonsterEncounter()
+        {
+            Console.WriteLine("A vicious monster appears!");
+            Console.ReadKey();
+        }
+        static void MonsterAttacks(int dmg)
+        {
+            playerHealth -= monsterDamage;
+          
+            Console.WriteLine($"The monster attacks you for {dmg} points of damage.You have {playerHealth} health left.");
+
+            Console.ReadKey();
+        }
+    }
+}
