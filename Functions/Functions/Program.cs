d4621f2e4db326ba491f817b0a675fb017428f0c
Joseph Huskey <josephhuskey@Josephs-MacBook-Air.local>
2020-08-14 11:06:59 -0700

    Fixed a string

--- Functions/Functions/Program.cs
+++ Functions/Functions/Program.cs
@@ -4,34 +4,29 @@ namespace Functions
 {
     class Program
     {
-        static int playerHealth = 100;
-        static int monsterDamage = 30;
-
-        static void Main(string[] args)
-        {
-            Start();
-        }
+        static int numberOne = 10;
+        static int numberTwo = 200;
         static void Start()
         {
-            Console.WriteLine("The game has started.");
-            Console.ReadKey();
-            MonsterEncounter();
-            MonsterAttacks(monsterDamage*2);
-            MonsterAttacks(monsterDamage);
-            MonsterAttacks(monsterDamage*3);
-        }
-        static void MonsterEncounter()
-        {
-            Console.WriteLine("A vicious monster appears!");
+            int biggestNumber = ReturnBiggestNumber(numberOne, numberTwo);
+            Console.WriteLine("Of " + numberOne + " and " + numberTwo + " the biggest number is " + biggestNumber);
+           
             Console.ReadKey();
         }
-        static void MonsterAttacks(int dmg)
+        static int ReturnBiggestNumber(int first, int second)
         {
-            playerHealth -= monsterDamage;
-          
-            Console.WriteLine($"The monster attacks you for {dmg} points of damage.You have {playerHealth} health left.");
-
-            Console.ReadKey();
+            if (first > second)
+            {
+                return first;
+            }
+            else
+            {
+                return second;
+            }
+        }
+        static void Main()
+        {
+            Start();
         }
     }
 }
