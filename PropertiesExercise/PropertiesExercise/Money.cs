b347f12f54e6c29474c5cd8c94b66cf195626b62
Joseph Huskey <josephhuskey@Josephs-MacBook-Air.local>
2020-08-14 14:43:51 -0700

    Finished properties exercise

---
+++ PropertiesExercise/PropertiesExercise/Money.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace PropertiesExercise
+{
+    public class Money
+    {
+        private float balance;
+        public float USDBalance
+        {
+            get
+            {
+                return balance * .8f;
+            }
+        }
+        public float AUDBalance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+
+        public void AddMoney(float add)
+        {
+            AUDBalance += add;
+        }
+        public void LoseMoney(float loss)
+        {
+            AUDBalance -= loss;
+        }
+            
+        
+   
+    }
+}
---
+++ PropertiesExercise/PropertiesExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PropertiesExercise
+{
+    class Program
+    {
+        static void Main()
+        {
+            Money money = new Money();
+            money.AUDBalance = 50.0f;
+            Console.WriteLine("The US Dollar amount is " + money.USDBalance);
+            Console.WriteLine("The Australian Dollar amount is " + money.AUDBalance);
+            Console.ReadKey();
+        }
+    }
+
+    
+       
+    
+}
