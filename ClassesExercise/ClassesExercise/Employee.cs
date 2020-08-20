5385243589b41a1cac987554a85dc3abfafe9c91
Joseph Huskey <josephhuskey@Josephs-MacBook-Air.local>
2020-08-19 17:38:49 -0700

    added classes exercise, did not do the array portion will update thurs.

---
+++ ClassesExercise/ClassesExercise/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ClassesExercise
+{
+    public class Employee
+    {
+       
+        public string firstName;
+        public string lastName;
+        public int yearsOfExperience;
+
+       
+        public void Member()
+        {
+            Console.WriteLine($"The employee is named {firstName} {lastName} and they have {yearsOfExperience} years of experience.");
+        }
+    }
+}
---
+++ ClassesExercise/ClassesExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee Jim = new Employee();
+            Jim.firstName = "jim";
+            Jim.lastName = "john";
+            Jim.yearsOfExperience = 5;
+            Jim.Member();
+            Console.WriteLine();
+        }
+    }
+   
+}
