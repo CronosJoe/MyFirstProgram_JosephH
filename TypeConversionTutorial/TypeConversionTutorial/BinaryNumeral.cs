a3b26b498a02211267f59310c2e432ba7706e643
Joseph Huskey <josephhuskey@Josephs-MacBook-Air.local>
2020-08-27 12:43:41 -0700

    finished roman numeral and binary conversions

---
+++ TypeConversionTutorial/TypeConversionTutorial/BinaryNumeral.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace TypeConversionTutorial
+{
+    public class BinaryNumeral
+    {
+        private int value;
+        public BinaryNumeral(int value)
+        {
+            this.value = value;
+        }
+        static public implicit operator BinaryNumeral(int value)
+        {
+            return new BinaryNumeral(value);
+        }
+        static public explicit operator int(BinaryNumeral binary)
+        {
+            return binary.value;
+        }
+        static public implicit operator string(BinaryNumeral binary)
+        {
+            string convers = "";
+            int n = binary.value;
+            int i;
+            int[] a = new int[10];
+            for ( i = 0; n > 0; i++)
+            {
+                a[i] = n % 2;
+                n = n / 2;
+            }
+            for (i = i - 1; i >= 0; i--)
+            {
+               
+                convers += a[i];
+            }
+            return convers;
+        }
+    }
+}
---
+++ TypeConversionTutorial/TypeConversionTutorial/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace TypeConversionTutorial
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+
+            RomanNumeral numeral;
+            numeral = 10;
+            //int
+            Console.WriteLine((int)numeral);
+            //string
+            Console.WriteLine(numeral);
+            
+            short s = (short)numeral;
+            Console.WriteLine(s);
+
+            BinaryNumeral number;
+            number = 10;
+            //int
+            Console.WriteLine((int)number);
+           //string
+            Console.WriteLine(number);
+           
+            short t = (short)number;
+
+            
+            Console.WriteLine(t);
+
+            RomanNumeral roman;
+            roman = 10;
+            BinaryNumeral binary;
+            // Perform a conversion from a RomanNumeral to a
+            // BinaryNumeral:
+            binary = (BinaryNumeral)(int)roman;
+            // Performs a conversion from a BinaryNumeral to a RomanNumeral.
+            // No cast is required:
+            roman = binary;
+            Console.WriteLine((int)binary);
+            Console.WriteLine(binary);
+            Console.ReadKey();
+        }
+    }
+
+    
+}
